7f37315b5385011da8f65f721c8d803d6c17be1a
Jerry <c1495616@gmail.com>
2017-01-02 12:34:01 +0800

    mod

--- src/app.jsx
+++ src/app.jsx
@@ -22,7 +22,7 @@ var config = {
     messagingSenderId: "554509220665"
   };
 firebase.initializeApp(config);
-firebase.database.ref().set({
+firebase.database().ref().set({
   appName:'Weather_Robot'      
 });
 render(
